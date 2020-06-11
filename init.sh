d0bf210085d73b33702b8ce0bfbdc6e51d6c2609
Julia010160 <59804083+Julia010160@users.noreply.github.com>
2020-06-11 19:11:10 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
-sudo ln -sf  /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx start
+sudo ln -sf /home/box/web/etc/nginx.conf
+sudo /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+
